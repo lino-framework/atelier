8fa6515ad14bd58f1015b8c5d05d8807ce0da6e7
khchine5 <hamzakhchine@gmail.com>
2016-01-16 21:49:23 +0100

    Add more Invoke command (pd,blog,ci,reg,mm) #357

--- atelier/tasks.py
+++ atelier/tasks.py
@@ -15,6 +15,66 @@ to manage my Python projects.
 Documenting
 -----------
 
+.. command:: invoke blog
+
+    Edit today's blog entry, create an empty file if it doesn't yet exist.
+
+.. command:: invoke bd
+
+    Build docs. Build all Sphinx HTML doctrees for this project.
+
+    This runs :cmd:`invoke readme`, followed by `sphinx-build html` in
+    every directory defined in :attr:`env.doc_trees`.  The exact
+    options for `sphinx-build` depend also on
+    :attr:`env.tolerate_sphinx_warnings` and :attr:`env.use_dirhtml`.
+
+.. command:: invoke pd
+
+    Publish docs. Upload docs to public web server.
+
+
+.. command:: invoke clean
+
+    Remove temporary and generated files:
+
+    - Sphinx `.build` files
+    - Dangling `.pyc` files which don't have a corresponding `.py` file.
+    - `cache` directories of demo projects
+    - additional files specified in :attr:`env.cleanable_files`
+
+Internationalization
+--------------------
+
+.. command:: invoke mm
+
+    ("make messages")
+
+    Extracts messages from both code and userdocs, then initializes and
+    updates all catalogs. Needs :attr:`env.locale_dir`
+
+Deploy
+------
+
+.. command:: invoke ci
+
+    Checkin and push to repository, using today's blog entry as commit
+    message.
+
+    Asks confirmation before doing so.
+
+    Does nothing in a project whose
+    :attr:`env.revision_control_system` is `None`.
+
+    In a project whose :attr:`env.revision_control_system` is
+    ``'git'`` it checks whether the repository is dirty (i.e. has
+    uncommitted changes) and returns without asking confirmation if
+    the repo is clean.  Note that unlike ``git status``, this check
+    does currently not (yet) check whether my branch is up-to-date
+    with 'origin/master'.
+
+.. command:: invoke reg
+
+    Register this project (and its current version) to PyPI.
 
 Testing
 -------
@@ -27,10 +87,32 @@ Testing
 
     See :func:`run_tests`.
 
+Installation
+============
 
+To be used by creating a :file:`tasks.py` in your project's root
+directory with at least the following two lines::
+
+  from atelier.tasks import *
+  env.setup_from_tasks(globals())
+
+See :func:`setup_from_tasks` for more information.
+
+Configuration files
+===================
+
+.. xfile:: tasks.py
+
+In your :xfile:`tasks.py` file you can specify project-specific
+configuration settings.  Example content::
+
+  from atelier.tasks import *
+  env.setup_from_tasks(globals(), "foobar")
+  env.languages = "de fr et nl".split()
+  env.tolerate_sphinx_warnings = True
+  add_demo_project('foobar.demo')
 
 """
-
 
 from __future__ import print_function
 import importlib
@@ -38,7 +120,11 @@ import importlib
 import os
 from contextlib import contextmanager
 import glob
-# from importlib import import_module
+import datetime
+from atelier.utils import i2d
+from babel.dates import format_date
+
+from atelier import rstgen
 
 from unipath import Path
 from invoke import task
@@ -52,6 +138,15 @@ def cd(path):
     os.chdir(path)
     yield
     os.chdir(old_dir)
+
+
+def get_current_date(today=None):
+    """
+    """
+
+    if today is None:
+        return datetime.date.today()
+    return i2d(today)
 
 
 def must_confirm(*args, **kwargs):
@@ -88,7 +183,6 @@ def cleanup_pyc(p):
 
 
 class Atelier(AttrDict):
-
     def setup_from_tasks(
             env, globals_dict, main_package=None, settings_module_name=None):
         if '__file__' not in globals_dict:
@@ -100,14 +194,15 @@ class Atelier(AttrDict):
         if not tasks.exists():
             raise Exception("No such file: %s" % tasks)
         defaults = dict(
-            root_dir = tasks.parent.absolute(),
-            main_package = main_package,
-            locale_dir = None,
-            tolerate_sphinx_warnings = False,
-            demo_projects = [],
-            revision_control_system = None,
-            apidoc_exclude_pathnames = [],
-            project_name = tasks.parent.absolute().name)
+            root_dir=tasks.parent.absolute(),
+            main_package=main_package,
+            locale_dir=None,
+            tolerate_sphinx_warnings=False,
+            demo_projects=[],
+            revision_control_system=None,
+            apidoc_exclude_pathnames=[],
+            project_name=tasks.parent.absolute().name,
+            editor_command=None)
 
         env.update(defaults)
 
@@ -241,7 +336,26 @@ class Atelier(AttrDict):
             with cd(build_dir):
                 local(build_dir_cmd)
 
+
 env = Atelier()
+
+
+class RstFile(object):
+    def __init__(self, local_root, url_root, parts):
+        self.path = local_root.child(*parts) + '.rst'
+        self.url = url_root + "/" + "/".join(parts) + '.html'
+        # if parts[0] == 'docs':
+        #     self.url = url_root + "/" + "/".join(parts[1:]) + '.html'
+        # else:
+        #     raise Exception("20131125")
+        # self.url = url_root + "/" + "/".join(parts) + '.html'
+
+
+class MissingConfig(Exception):
+    def __init__(self, name):
+        msg = "Must set `env.{0}` in `tasks.py`!"
+        msg = msg.format(name)
+        Exception.__init__(self, msg)
 
 
 @task(name='initdb')
@@ -298,9 +412,9 @@ Read more on %(url)s
     docs_index = env.root_dir.child('docs', 'index.rst')
     if docs_index.exists():
         docs_index.set_times()
-    # cmd = "touch " + env.DOCSDIR.child('index.rst')
-    # local(cmd)
-    # pypi_register()
+        # cmd = "touch " + env.DOCSDIR.child('index.rst')
+        # local(cmd)
+        # pypi_register()
 
 
 @task(write_readme, name='bd')
@@ -323,25 +437,26 @@ def clean(*cmdline_args):
     env.py_clean()
     # clean_demo_caches()
 
+
 @task(name='cov')
 def run_tests_coverage():
     """
     Run all tests, creating coverage report
     """
     import coverage
-    #~ clean_sys_path()
+    # ~ clean_sys_path()
     print("Running tests for '%s' within coverage..." % env.project_name)
-    #~ env.DOCSDIR.chdir()
+    # ~ env.DOCSDIR.chdir()
     source = []
     env.current_project.load_tasks()
     for package_name in env.current_project.SETUP_INFO['packages']:
         m = importlib.import_module(package_name)
         source.append(os.path.dirname(m.__file__))
-    #~ cov = coverage.coverage(source=['djangosite'])
+    # ~ cov = coverage.coverage(source=['djangosite'])
     if not confirm("coverage source=%s" % source):
         exit()
-    cov = coverage.coverage(source=source,)
-    #~ cov = coverage.coverage()
+    cov = coverage.coverage(source=source, )
+    # ~ cov = coverage.coverage()
     cov.start()
 
     # .. call your code ..
@@ -353,3 +468,238 @@ def run_tests_coverage():
     cov.html_report()
     local('coverage report')
     return rv
+
+
+@task(name='mm')
+def make_messages():
+    "Extract messages, then initialize and update all catalogs."
+    extract_messages()
+    init_catalog_code()
+    update_catalog_code()
+
+    # if False:
+    #     pass
+    # extract_messages_userdocs()
+    # setup_babel_userdocs('init_catalog')
+    # setup_babel_userdocs('update_catalog')
+
+
+@task(name='reg')
+def pypi_register():
+    """See :cmd:`fab reg`. """
+    args = ["python", "setup.py"]
+    args += ["register"]
+    # ~ run_setup('setup.py',args)
+    local(' '.join(args))
+
+
+@task(name='ci')
+def checkin(today=None):
+    """See :cmd:`fab ci`. """
+
+    if env.revision_control_system is None:
+        return
+
+    if env.revision_control_system == 'git':
+        from git import Repo
+        repo = Repo(env.root_dir)
+        if not repo.is_dirty():
+            print("No changes to commit in {0}.".format(env.root_dir))
+            return
+
+    show_revision_status()
+
+    today = get_current_date(today)
+
+    entry = get_blog_entry(today)
+    if not entry.path.exists():
+        quit("%s does not exist!" % entry.path.absolute())
+
+    msg = entry.url
+
+    if not confirm("OK to checkin %s %s?" % (env.project_name, msg)):
+        return
+    # ~ puts("Commit message refers to %s" % entry.absolute())
+
+    if env.revision_control_system == 'hg':
+        args = ["hg", "ci"]
+    else:
+        args = ["git", "commit", "-a"]
+    args += ['-m', msg]
+    cmd = ' '.join(args)
+    local(cmd)
+    if env.revision_control_system == 'hg':
+        local("hg push %s" % env.project_name)
+    else:
+        local("git push")
+
+
+@task(name='blog')
+def edit_blog_entry(today=None):
+    """Edit today's blog entry, create an empty file if it doesn't yet exist.
+
+    :today: Useful when a working day lasted longer than midnight, or
+            when you start some work in the evening, knowing that you
+            won't commit it before the next morning.  Note that you
+            must specify the date using the YYYYMMDD format.
+
+            Usage example::
+
+                $ fab blog:20150727
+
+    """
+    if not env.editor_command:
+        raise MissingConfig("editor_command")
+    today = get_current_date(today)
+    entry = get_blog_entry(today)
+    if not entry.path.exists():
+        if not confirm("Create file %s?" % entry.path):
+            return
+        # for every year we create a new directory.
+        yd = entry.path.parent
+        if not yd.exists():
+            if not confirm("Happy New Year! Create directory %s?" % yd):
+                return
+            yd.mkdir()
+            txt = ".. blogger_year::\n"
+            yd.child('index.rst').write_file(txt.encode('utf-8'))
+
+        if env.languages is None:
+            txt = today.strftime(env.long_date_format)
+        else:
+            txt = format_date(
+                today, format='full', locale=env.languages[0])
+        entry.path.write_file(rstgen.header(1, txt).encode('utf-8'))
+        # touch it for Sphinx:
+        entry.path.parent.child('index.rst').set_times()
+    args = [env.editor_command]
+    args += [entry.path]
+    local(' '.join(args))
+
+@task(name='pd')
+def publish():
+    """See :cmd:`fab pd`. """
+    if not env.docs_rsync_dest:
+        raise MissingConfig("docs_rsync_dest")
+
+    for docs_dir in get_doc_trees():
+        build_dir = docs_dir.child(env.build_dir_name)
+        if build_dir.exists():
+            name = '%s_%s' % (env.project_name, docs_dir.name)
+            dest_url = env.docs_rsync_dest % name
+            publish_docs(build_dir, dest_url)
+
+    # build_dir = env.root_dir.child('userdocs', env.build_dir_name)
+    # if build_dir.exists():
+    #     dest_url = env.docs_rsync_dest % (env.project_name + '-userdocs')
+    #     publish_docs(build_dir, dest_url)
+
+def get_doc_trees():
+    for rel_doc_tree in env.doc_trees:
+        docs_dir = env.root_dir.child(rel_doc_tree)
+        if not docs_dir.exists():
+            msg = "Directory %s does not exist." % docs_dir
+            msg += "\nCheck your project's `doc_trees` setting."
+            raise Exception(msg)
+        yield docs_dir
+
+def publish_docs(build_dir, dest_url):
+    with cd(build_dir):
+        args = ['rsync', '-r']
+        args += ['--verbose']
+        args += ['--progress']  # show progress
+        args += ['--delete']  # delete files in dest
+        args += ['--times']  # preserve timestamps
+        args += ['--exclude', '.doctrees']
+        args += ['./']  # source
+        args += [dest_url]  # dest
+        cmd = ' '.join(args)
+        #~ must_confirm("%s> %s" % (build_dir, cmd))
+        #~ confirm("yes")
+        local(cmd)
+
+
+def show_revision_status():
+    if env.revision_control_system == 'hg':
+        args = ["hg", "st"]
+    elif env.revision_control_system == 'git':
+        args = ["git", "status"]
+    else:
+        print("Invalid revision_control_system %r !" %
+              env.revision_control_system)
+        return
+    print("-" * 80)
+    local(' '.join(args))
+    print("-" * 80)
+
+
+def get_blog_entry(today):
+    """Return an RstFile object representing the blog entry for that date
+    in the current project.
+
+    """
+    parts = ('blog', str(today.year), today.strftime("%m%d"))
+    return RstFile(Path(env.blog_root), env.blogref_url, parts)
+
+
+def extract_messages():
+    """Extract messages from source files to `django.pot` file"""
+    # locale_dir = get_locale_dir()
+    locale_dir = env.locale_dir
+    if locale_dir is None:
+        return
+    args = ["python", "setup.py"]
+    args += ["extract_messages"]
+    args += ["-o", Path(locale_dir).child("django.pot")]
+    cmd = ' '.join(args)
+    # ~ must_confirm(cmd)
+    local(cmd)
+
+
+def init_catalog_code():
+    """Create code .po files if necessary."""
+    from lino.core.site import to_locale
+    locale_dir = env.locale_dir
+    # locale_dir = get_locale_dir()
+    if locale_dir is None:
+        return
+    locale_dir = Path(locale_dir)
+    for loc in env.languages:
+        if loc != 'en':
+            f = locale_dir.child(loc, 'LC_MESSAGES', 'django.po')
+            if f.exists():
+                print
+                "Skip %s because file exists." % f
+            else:
+                args = ["python", "setup.py"]
+                args += ["init_catalog"]
+                args += ["--domain django"]
+                args += ["-l", to_locale(loc)]
+                args += ["-d", locale_dir]
+                # ~ args += [ "-o" , f ]
+                args += ["-i", locale_dir.child('django.pot')]
+                cmd = ' '.join(args)
+                must_confirm(cmd)
+                local(cmd)
+
+
+def update_catalog_code():
+    """Update .po files from .pot file."""
+    from lino.core.site import to_locale
+    locale_dir = env.locale_dir
+    # locale_dir = get_locale_dir()
+    if locale_dir is None:
+        return
+    locale_dir = Path(locale_dir)
+    for loc in env.languages:
+        if loc != env.languages[0]:
+            args = ["python", "setup.py"]
+            args += ["update_catalog"]
+            args += ["--domain django"]
+            # ~ args += [ "-d" , locale_dir ]
+            args += ["-o", locale_dir.child(loc, 'LC_MESSAGES', 'django.po')]
+            args += ["-i", locale_dir.child("django.pot")]
+            args += ["-l", to_locale(loc)]
+            cmd = ' '.join(args)
+            # ~ must_confirm(cmd)
+            local(cmd)
