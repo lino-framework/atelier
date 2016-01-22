0934b22e9e87db85639996304a2520a9e24f1571
khchine5 <hamzakhchine@gmail.com>
2016-01-22 22:24:34 +0100

    Fix getting  ctx config such ctx.main_package #357

--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
 #from atelier import
-#print '20160121a', globals().keys()
 from atelier.inv import ns, setup_from_tasks
 setup_from_tasks(globals(), "atelier")
 revision_control_system = 'git'
-#print '20160121b', globals().keys()
