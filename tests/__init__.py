83cf12fafa5c1718d72352c685d615f60f85ef14
Luc Saffre <luc.saffre@gmail.com>
2016-07-16 13:16:34 +0300

    http://luc.lino-framework.org/blog/2016/0716.html

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,17 @@
-from unipath import Path
-
-ROOTDIR = Path(__file__).parent.parent
+# -*- coding: UTF-8 -*-
 
 from atelier import SETUP_INFO
 from atelier.test import TestCase
 
 
-class BaseTestCase(TestCase):
-    project_root = ROOTDIR
-    
-
-class BasicTests(BaseTestCase):
+class BasicTests(TestCase):
 
     def test_01(self):
         self.assertEqual(1+1, 2)
+
+    # the following test case should always be commented out
+    # def test_fail_with_unicode_error(self):
+    #     self.fail(u"Scheiße wa!")
 
     def test_utils(self):
         self.run_simple_doctests('atelier/utils.py')
@@ -25,12 +23,12 @@ class BasicTests(BaseTestCase):
         self.run_simple_doctests('atelier/rstgen.py')
 
 
-class PackagesTests(BaseTestCase):
+class PackagesTests(TestCase):
     def test_packages(self):
         self.run_packages_test(SETUP_INFO['packages'])
 
 
-class SphinxTests(BaseTestCase):
+class SphinxTests(TestCase):
     def test_sphinxconf(self):
         self.run_simple_doctests('atelier/sphinxconf/__init__.py')
 
