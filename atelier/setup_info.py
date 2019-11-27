3ae000dd7a239b694bb5364b2c9b6c89b1ece73c
Hamza Khchine <hamzakhchine@gmail.com>
2019-11-27 10:53:52 +0100

    Release 1.1.23

--- atelier/setup_info.py
+++ atelier/setup_info.py
@@ -22,7 +22,7 @@ except ImportError:
 
 SETUP_INFO = dict(
     name='atelier',
-    version='1.1.22',
+    version='1.1.23',
     install_requires=install_requires,
     tests_require=tests_require,
     scripts=['scripts/per_project'],
