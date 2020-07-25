e72e2fac169c3c9517f691d70382768d9ab5ebc5
Luc Saffre <luc.saffre@gmail.com>
2020-07-25 22:11:37 +0300

    py2to3, optimize message format

--- atelier/invlib/__init__.py
+++ atelier/invlib/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2016-2018 Rumma & Ko Ltd
+# Copyright 2016-2020 Rumma & Ko Ltd
 # License: BSD, see LICENSE for more details.
 """
 A library of `invoke
@@ -12,10 +12,6 @@ A library of `invoke
    tasks
    utils
 """
-
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import unicode_literals
 
 import os
 import six
@@ -37,7 +33,7 @@ def setup_from_tasks(
     if '__file__' not in globals_dict:
         raise Exception(
             "No '__file__' in %r. "
-            "First parameter to must be `globals()`" % globals_dict)
+            "First argument to setup_from_tasks() must be `globals()`." % globals_dict)
 
     tasks_file = Path(globals_dict['__file__'])
     if not tasks_file.exists():
@@ -50,10 +46,10 @@ def setup_from_tasks(
 
     if atelier.current_project is None:
         atelier.current_project = prj
-    
+
     if kwargs:
         prj.config.update(kwargs)
-        
+
     if settings_module_name is not None:
         os.environ['DJANGO_SETTINGS_MODULE'] = settings_module_name
         from django.conf import settings
@@ -64,9 +60,8 @@ def setup_from_tasks(
         main_package = import_module(main_package)
     if main_package:
         prj.set_main_package(main_package)
-        
+
     self = Collection.from_module(tasks)
     prj.set_namespace(self)
 
     return self
-
