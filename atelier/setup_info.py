cded9ae46b78dbaa0284f363c5b5f0f4fe160169
Luc Saffre <luc.saffre@gmail.com>
2021-03-03 22:45:01 +0200

    release to pypi

--- atelier/setup_info.py
+++ atelier/setup_info.py
@@ -24,7 +24,7 @@ except ImportError:
 
 SETUP_INFO = dict(
     name='atelier',
-    version='1.1.27',
+    version='1.1.28',
     install_requires=install_requires,
     tests_require=tests_require,
     scripts=['scripts/per_project'],
